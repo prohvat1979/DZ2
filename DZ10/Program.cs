2ba1c9bdf26f6beb1a603f5286f4b8f10ec891a4
«Sergey» <notan1979@yandex.ru>
2023-06-17 16:27:10 +0300

    DZ10

---
+++ DZ10/Program.cs
@@ -0,0 +1,14 @@
+﻿// Принять на вход трехзначное число и вывести вторую цифру этого числа.
+
+Console.Write("Введите трёхзначное число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+if (numberA < 100 || numberA > 999)
+{
+    Console.WriteLine("Введённое число не трёхзначное!");
+}
+else
+{
+    int numberB = numberA / 10 % 10;
+    Console.WriteLine($"Вторая цифра введённого числа: {numberB}");
+}
