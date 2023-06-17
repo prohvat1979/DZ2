270a2fd8f9d7528d2b3e62c89f6955e7c5968663
«Sergey» <notan1979@yandex.ru>
2023-06-17 13:48:45 +0300

    DZ13

---
+++ DZ13/Program.cs
@@ -0,0 +1,28 @@
+﻿// Принять на ввод число и вывести третью цифру этого числа либо сообщить что третьей цифры нет.
+
+Console.Write("Введите число: ");
+string? input = Console.ReadLine();
+
+if (string.IsNullOrEmpty(input))
+{
+    Console.Error.WriteLine("Ничего не введено.");
+    return;
+}
+
+for (int i = 0; i < input.Length; i++)
+{
+    if (!char.IsDigit(input[i]))
+    {
+        Console.Error.WriteLine("Введено не число.");
+        return;
+    }
+}
+
+if (input.Length < 3)
+{
+    Console.WriteLine("Третьей цифры нет.");
+}
+else
+{
+    Console.WriteLine($"Третья цифра: {input[2]}");
+}
